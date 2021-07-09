eeef6c8e89f0cfd6e9aae7cf96e51da9232cd013
Zane-2020 <85378964+Zane-2020@users.noreply.github.com>
2021-07-09 01:04:40 -0400

    Update Inventory.sh

--- Inventory.sh
+++ Inventory.sh
@@ -5,3 +5,24 @@ if [ $? -eq o ]
 then
 echo " success"
 fi
+
+if [ -f /etc/group ]
+then
+echo " the file /etc/group exist"
+else
+echo " file  /etc/group is missing"
+fi
+
+if [ -f /etc/passwd ]
+then
+echo " the file /etc/passwd exist"
+else
+echo " the file /etc/passwd is missing"
+fi
+
+if [ -f /etc/grub.conf ]
+then
+echo " the file /etc/grub.conf exist"
+else
+echo " the file /etc/grub.conf is missing"
+fi
